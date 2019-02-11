22cd35e0fdb21dd3f4d56c24f323adf5cb43d52f
stevenyang98 <46117618+stevenyang98@users.noreply.github.com>
2019-02-11 12:49:53 -0500

    Update PisonCodingChallenge_Steven_Yang.py

--- Pison/PisonCodingChallenge_Steven_Yang.py
+++ Pison/PisonCodingChallenge_Steven_Yang.py
@@ -8,7 +8,7 @@ end = 3//2+1
 deltas = []
 for i in range(start, end):
     for j in range(start, end):
-        deltas.append((i, j))
+        deltas.append((i, j))  # create list of possible moves the account for
 
 
 def neighbors(x, y):
@@ -21,7 +21,7 @@ def neighbors(x, y):
         else:
             valid.append((x+i, y+j))
 
-    return valid
+    return valid  # account for boundary cases if move takes us of the board
 
 
 initial = {"valid": False}
@@ -32,7 +32,7 @@ def make_trie(word, trie):
         return None
 
     if word[0] not in trie:
-        trie[word[0]] = {"valid": len(word) == 1}
+        trie[word[0]] = {"valid": len(word) == 1}  # keep recursing until no more letters in the word, at the end, we flag with boolean
 
     return make_trie(word[1:], trie[word[0]])
 
@@ -40,10 +40,10 @@ def make_trie(word, trie):
 def gen_trie(words):
     for word in words:
         make_trie(word, initial)
-    return initial
+    return initial  # make trie for all words
 
 
-solutions = set()
+solutions = set() # valid set of words to return
 
 
 def dfs(x, y, visited, current, trie):
@@ -55,22 +55,23 @@ def dfs(x, y, visited, current, trie):
 
     if letter in trie:
         current += letter
-        if trie[letter]["valid"]:
+        if trie[letter]["valid"]:  #if we have a valid word, print and add to the set
             print("Valid word: ", current)
             solutions.add(current)
 
         for new_x, new_y in neighbors(x, y):
-            dfs(new_x, new_y, visited.copy(), current, trie[letter])
+            dfs(new_x, new_y, visited.copy(), current, trie[letter])  # recurse on all neighbors, our trie is a nested dictionary
+                                                                      # so we recurse on the trie
 
 
 def solution(trie):
     for i in range(length):
         for j in range(length):
-            dfs(i, j, set(), '', trie)
+            dfs(i, j, set(), '', trie)  # recurse on all spots in the board 
     return solutions
 
 
-print(solution(gen_trie(Words)))
+print(solution(gen_trie(Words)))  # prints out our solution
 
 
 
